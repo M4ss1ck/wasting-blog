5dd02263f6b5898275f92fc46f2af3361cb5bc47
M4ss1ck <4ndyraul@gmail.com>
2022-04-30 04:24:10 -0400

    sort classes

--- src/templates/default-page.js
+++ src/templates/default-page.js
@@ -5,15 +5,15 @@ function DefaultPageTemplate({ pageContext: { page } }) {
   return (
     <div className="divide-y divide-gray-200">
       <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-        <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           {page.title}
         </h1>
         {page.subtitle && (
-          <p className="text-lg leading-7 text-gray-500">{page.subtitle}</p>
+          <p className="text-lg leading-7 text-textcolor">{page.subtitle}</p>
         )}
       </div>
       <div className="pb-16 lg:pb-20">
-        <div className="prose dark:prose-dark max-w-none pt-10 pb-8">
+        <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">
           {page.content.markdownNode ? (
             <MDXRenderer>{page.content.markdownNode.childMdx.body}</MDXRenderer>
           ) : (
