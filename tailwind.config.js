0f8d35e067bc19bdeeb0dc61593c64fe67ef60d9
M4ss1ck <4ndyraul@gmail.com>
2022-04-30 02:31:29 -0400

    add telegram web apps variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  //mode: "jit",
   darkMode: "class", // it's 'media' by default as of v3
   content: ["./src/**/*.js"],
   theme: {
     extend: {
       colors: {
-        green: colors.emerald,
-        yellow: colors.amber,
-        purple: colors.violet,
+        tema: "var(--tg-color-scheme)",
+        bgcolor: "var(--tg-theme-bg-color)",
+        textcolor: "var(--tg-theme-text-color)",
+        hint: "var(--tg-theme-hint-color)",
+        link: "var(--tg-theme-link-color)",
+        button: "var(--tg-theme-button-color)",
+        buttontext: "var(--tg-theme-button-text-color)",
       },
       typography(theme) {
         return {
@@ -50,22 +53,9 @@ module.exports = {
         13: "3.25rem",
         14: "3.5rem",
       },
-      colors: {
-        primario: "#252f3f",
-        secundario: "#0276df",
-        terciario: "#02b3dd",
-      },
     },
   },
-  // variants: {
-  //   extend: {
-  //     typography: ["dark"],
-  //     display: ["responsive", "hover", "focus", "group-hover"],
-  //     animation: ["hover"],
-  //   },
-  // },
   plugins: [require("@tailwindcss/typography")],
-  //plugins: [require("@tailwindcss/ui")],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
